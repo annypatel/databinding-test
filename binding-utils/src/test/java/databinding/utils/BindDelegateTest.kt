9463b457059ce5e51f51f24ec7dd522f4010d826
Anny Patel <30823493+annypatel@users.noreply.github.com>
2019-03-18 15:41:30 +0530

    Add binding-utils module with BindDelegate

---
+++ binding-utils/src/test/java/databinding/utils/BindDelegateTest.kt
@@ -0,0 +1,42 @@
+package databinding.utils
+
+import androidx.databinding.BaseObservable
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import org.junit.Assert.assertThat
+import org.junit.Test
+import org.hamcrest.CoreMatchers.equalTo
+
+class BindDelegateTest {
+
+    @Test
+    fun getValue_givenDelegateWithData_shouldReturnData() {
+        val expectedValue = "Sample Data"
+        val delegate = BindDelegate(12, expectedValue)
+
+        val actualValue = delegate.getValue(mock(), mock())
+
+        assertThat(actualValue, equalTo(expectedValue))
+    }
+
+    @Test
+    fun setValue_givenDelegate_shouldSetData() {
+        val expectedValue = "Updated Data"
+        val delegate = BindDelegate(12, "Init Data")
+
+        delegate.setValue(mock(), mock(), expectedValue)
+
+        assertThat(delegate.getValue(mock(), mock()), equalTo(expectedValue))
+    }
+
+    @Test
+    fun setValue_givenDelegate_shouldNotifyPropertyChanged() {
+        val expectedFieldId = 12
+        val observable = mock<BaseObservable>()
+        val delegate = BindDelegate(expectedFieldId, "Init Data")
+
+        delegate.setValue(observable, mock(), "Updated Data")
+
+        verify(observable).notifyPropertyChanged(expectedFieldId)
+    }
+}
