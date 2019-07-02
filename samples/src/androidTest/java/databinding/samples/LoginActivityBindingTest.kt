24cbb0abebebbcf177f138faca3be2edc6ec9196
Anny Patel <30823493+annypatel@users.noreply.github.com>
2019-07-02 16:56:01 +0530

    Add test for login click in activity_login layout

--- samples/src/androidTest/java/databinding/samples/LoginActivityBindingTest.kt
+++ samples/src/androidTest/java/databinding/samples/LoginActivityBindingTest.kt
@@ -1,8 +1,12 @@
 package databinding.samples
 
+import android.os.Build.VERSION_CODES
 import androidx.test.annotation.UiThreadTest
 import androidx.test.espresso.matcher.ViewMatchers.assertThat
 import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.filters.SdkSuppress
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
 import databinding.test.utils.bindNow
 import databinding.test.utils.inflate
 import org.junit.Test
@@ -21,5 +25,16 @@ class LoginActivityBindingTest {
 
         assertThat(etUserName, withText("brucewayne"))
         assertThat(etPassport, withText("123!@#"))
+    }
+
+    @Test
+    @UiThreadTest
+    @SdkSuppress(minSdkVersion = VERSION_CODES.P)
+    fun clickOnLoginButton() = inflate<LoginActivityBinding>(R.layout.activity_login) {
+        viewModel = mock()
+
+        btnLogin.performClick()
+
+        verify(viewModel)?.login()
     }
 }
